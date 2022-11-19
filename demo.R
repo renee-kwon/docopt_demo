0c3f4d32277144abbd4bbb0cd7558c36243d13f8
renee-kwon <112587916+renee-kwon@users.noreply.github.com>
2022-11-18 16:57:52 -0800

    add optional positional requirement

--- demo.R
+++ demo.R
@@ -4,12 +4,13 @@
 main <- function(){
   
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [--arg4=<arg4>] 
+Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [<arg4>] 
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[<arg4>]          Takes any value (this is an optional positional requirement option)
 " -> doc
 
 library(docopt)
